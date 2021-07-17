a4a5bfd9616d9d9c0c191c4a11c1bc12d50896bf
ShafaetZamanShuvo <1705086@ugrad.cse.buet.ac.bd>
2021-07-17 22:27:55 +0600

    We can now shoot and it increases scores. but buggy

--- Shooting Stars/Game/Game/main.cpp
+++ Shooting Stars/Game/Game/main.cpp
@@ -22,10 +22,17 @@
 //row hoitese upor theke niche 1 theke 16
 // but column hoitese bam theke dan e 1 theke 16
 int LIFE_LEFT = 5;
+int GAME_SCORE = 0;
 int count = 0;
+int firehasCalled = 0;
+int fireIsActive = 0;
+int score;
 char life_level[10];
-volatile int bulletColPosition, bulletRowPosition; //
-volatile int playerRowPosition, playerColPosition; //
+char score_count[10];
+int bullet_hit_by_fire;
+volatile int bulletColPosition, bulletRowPosition;
+volatile int playerRowPosition, playerColPosition;
+volatile int fireColPosition, fireRowPosition;
 int enemyPosition;
 
 struct Player {
@@ -36,7 +43,33 @@ struct Enemy {
 	int enemyRow, enemyCol, enemyLength;
 }enemyistiak;
 
-ISR(INT0_vect){	if(playerColPosition>2) playerColPosition--;	//else playerColPosition = 2;	//if(count < 11) {count ++;}}ISR(INT1_vect){	if(playerColPosition<15) playerColPosition++;	//else playerColPosition = 2;	//if(count > 0) {count --;}}
+struct Fire
+{
+	int firerow, firecol;
+} fireplayer;
+
+ISR(INT0_vect)
+{
+	if(playerColPosition>2) playerColPosition--;
+	//else playerColPosition = 2;
+	//if(count < 11) {count ++;}
+}
+ISR(INT1_vect)
+{
+	if(playerColPosition<15) playerColPosition++;
+	//else playerColPosition = 2;
+	//if(count > 0) {count --;}
+}
+
+ISR(INT2_vect)
+{
+	if (firehasCalled == 0)
+	{
+		firehasCalled = 1;
+		//fireRowPosition = playerRowPosition;
+		//fireColPosition = playerColPosition;
+	}
+}
 
 int bulletHit()
 {
@@ -51,6 +84,8 @@ int bulletHit()
 	return 0;
 }
 
+
+
 void portInit()
 {
 	DDRA = 0xFF;
@@ -62,8 +97,10 @@ void portInit()
 	enemyistiak.enemyRow = 1;
 	enemyistiak.enemyCol = 2;
 	playerRowPosition = 16;
-	playerColPosition = 2;
+	playerColPosition = 8;
 	bulletRowPosition = 1;
+	bullet_hit_by_fire = 0;
+	score =0;
 }
 
 char col_position0[] = {0b11100000, 0b01110000, 0b00111000,0b00011100,0b00001110,0b00000111,0b00000000, 0b00000000,0b00000000, 0b00000000,0b00000000,0b00000000};
@@ -98,29 +135,42 @@ void showLed(int row, int column)
 	{
 		rowcode = row_position3[row-1];
 	}
-	
 	PORTA = columncode;
 	PORTB = rowcode;
-	
 }
 
-void initialize_canon(){	showLed(16,1);	_delay_ms(7);	showLed(16,2);	_delay_ms(7);	showLed(16,3);	_delay_ms(7);	showLed(15,1);	_delay_ms(7);	showLed(15,2);	_delay_ms(7);	showLed(15,3);	_delay_ms(7);	showLed(14,2);	_delay_ms(7);}
 
 
 void playerMove()
 {
 	shafaet.playerRow = playerRowPosition;
 	shafaet.playerCol = playerColPosition;
-	//_delay_ms(5);
+	showLed(shafaet.playerRow-2, shafaet.playerCol);
+	_delay_ms(5);
+	showLed(shafaet.playerRow-1, shafaet.playerCol-1);
+	_delay_ms(5);
+	showLed(shafaet.playerRow-1, shafaet.playerCol);
+	_delay_ms(5);
+	showLed(shafaet.playerRow-1, shafaet.playerCol+1);
+	_delay_ms(5);
 	showLed(shafaet.playerRow, shafaet.playerCol-1);
 	_delay_ms(5);
 	showLed(shafaet.playerRow, shafaet.playerCol);
 	_delay_ms(5);
 	showLed(shafaet.playerRow, shafaet.playerCol+1);
 	_delay_ms(5);
-	//shafaet.playerCol = (shafaet.playerCol+1)%16;
-	//playerRowPosition++;
-	//playerColPosition++;
+
+}
+
+void fireMove()
+{
+	fireplayer.firecol = fireColPosition;
+	fireplayer.firerow = fireRowPosition;
+	showLed(fireplayer.firerow-1,fireplayer.firecol);
+	_delay_ms(10);
+	showLed(fireplayer.firerow,fireplayer.firecol);
+	_delay_ms(10);
+	
 }
 
 void enemyMove(int count)
@@ -133,83 +183,116 @@ void enemyMove(int count)
 	_delay_ms(5);
 	showLed(enemyistiak.enemyRow, enemyistiak.enemyCol+1);
 	_delay_ms(5);
-	//enemyistiak.enemyCol = (enemyistiak.enemyCol+1)%16;
 }
-
-void bulletRain(){	int col;	if(bulletRowPosition ==1) {		col= rand()%17+1;		bulletColPosition = col;	}	if(bulletRowPosition!=1)	{		//col= rand()%17+1;		col = bulletColPosition ;	}	else col = bulletColPosition;		showLed(bulletRowPosition,col);	if(bulletHit()) LIFE_LEFT--;	for(int i =1; i<10; i++)	{		_delay_ms(5);		playerMove();		_delay_ms(5);	}		//_delay_ms(100);	if(bulletRowPosition==16)	{		bulletRowPosition = 1;	}	else bulletRowPosition++;}
-
-void bulletRain2(){		int col;	col= rand()%17+1;	for(int row = 1; row <= 16; row++)	{		showLed(row,col);		_delay_ms(100);	}	}
-
-
-void display(int count)
+void fireBulletCollision()
 {
-	if (count < 6)
+	if (bulletColPosition == fireColPosition  && bulletRowPosition == fireRowPosition)
 	{
-		PORTA = col_position0[count];
-		PORTB = 0b00111001;
-		_delay_ms(1000);
-		//bulletRain(count);
-		
+		score++;
+		Lcd4_Write_String(" Hit");
+		_delay_ms(100);
+		Lcd4_Write_String(" ");
+		_delay_ms(100);
+		bullet_hit_by_fire = 1;
+		bulletRowPosition = 0;
+		bulletColPosition = 0;
+		for (int i = 1; i<= 30; i++)
+		{
+			playerMove();
+		}
+
 		
 	}
-	else
-	{
-		PORTA = col_position1[count];
-		PORTB = 0b00111000;
-		_delay_ms(1000);
-		//bulletRain(count);
-		
-		
-	}
+	
 }
+
+void bulletRain()
+{
+	int col;
+	 if(bulletRowPosition ==1) {
+		//col= rand()%17+1;
+		//for testing
+		col = 8;
+		bulletColPosition = col;
+	}
+	else if(bulletRowPosition!=1)
+	{
+		col = bulletColPosition ;
+	}
+	else col = bulletColPosition;
+	
+	showLed(bulletRowPosition,col);
+	_delay_ms(10);
+	if (firehasCalled == 1 && fireIsActive == 0)
+	{
+		fireRowPosition = playerRowPosition + 3;
+		fireColPosition = playerColPosition;
+		fireIsActive = 1;
+		fireMove();
+		_delay_ms(10);
+	}
+	else if(fireIsActive == 1)
+	{
+		fireBulletCollision();
+		fireMove();
+	}
+	if(bulletHit()) LIFE_LEFT--;
+	for(int i =1; i<=10; i++)
+	{
+		
+		playerMove();
+	}
+	
+	if(bulletRowPosition==16)
+	{
+		bulletRowPosition = 1;
+	}
+
+	else {
+		bulletRowPosition++;
+	}
+	
+	if(fireIsActive == 1)
+	{
+		if(fireRowPosition==1)
+		{
+			fireIsActive = 0;
+			firehasCalled = 0;
+		}
+		else
+		{
+			fireRowPosition = fireRowPosition - 1;
+		}
+	}
+	
+}
+
 
 int main()
 {
-	MCUCR = MCUCR |  0b00000011;	GICR = (1 << INT1) | (1 << INT0 );	sei();
+	MCUCR = MCUCR |  0b00000011;
+	MCUCSR = MCUCSR |0b01000000;
+	
+	GICR = (1 << INT1) | (1 << INT0 ) | (1 << INT2);
+	sei();
 	portInit();
 	Lcd4_Init();
-	/*
-	for (int i = 1; i<= 16; i++)
-	{
-		for (int j = 1; j<= 16; j++)
-		
-		{
-			showLed(i,j);
-			_delay_ms(100);
-		}
-		_delay_ms(10);
-	}*/
+
 	while(1)
 	{
-		/*
-		Lcd4_Set_Cursor(1,1);
-		Lcd4_Write_Char('S');
-		Lcd4_Write_Char('c');
-		Lcd4_Write_Char('o');
-		Lcd4_Write_Char('r');
-		Lcd4_Write_Char('e');
-		Lcd4_Write_Char(':');
-		Lcd4_Write_Char('0');
 		
-		Lcd4_Set_Cursor(2,1);
-		Lcd4_Write_Char('L');
-		Lcd4_Write_Char('i');
-		Lcd4_Write_Char('v');
-		Lcd4_Write_Char('e');
-		Lcd4_Write_Char(':');
-		Lcd4_Write_Char('5');
-		*/
 		
 		sprintf(life_level, "%d", LIFE_LEFT);
+		sprintf(score_count,"%d", score);
 		Lcd4_Set_Cursor(1,1);
-		Lcd4_Write_String("Life left : ");
+		Lcd4_Write_String("Life: ");
 		Lcd4_Write_String(life_level);
-		//initialize_canon();
-		//playerMove();
+		Lcd4_Set_Cursor(2,1);
+		Lcd4_Write_String("Score: ");
+		Lcd4_Write_String(score_count);
+		
 		bulletRain();
 		if(LIFE_LEFT ==0) break;
-		//enemyMove(i);
-		//bulletRain2();
 	}
 	Lcd4_Init();
 	Lcd4_Set_Cursor(2,1);
